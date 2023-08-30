4505754ba5c23fc98cc8c4e9ef65a0598f72e244
hasoleee <wsw3727@gmail.com>
2023-08-31 00:55:44 +0900

    7. state

--- 01react/src/App.js
+++ 01react/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 
 function Article(props){
   return <article>
@@ -21,12 +21,13 @@ function Nav(props){
   const lis=[]
   for (let i=0; i<props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}>
+    lis.push(
+      <li key={t.id}>
       <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
         event.preventDefault();
         props.onChangeMode(event.target.id);
-      }}>{t.title}</a>
-      {t.body}
+      }}>{t.title}
+      </a>
       </li>);
   }
   return <nav>
@@ -36,20 +37,33 @@ function Nav(props){
   </nav>
 }
 function App() {
+  //const _mode = useState ('WELCOME'); //초기값
+  //const mode = _mode[0]; //상태의 값을 읽음
+  //const setMode = _mode[1]; //setmode를 통해서 값을 바꿈
+
+  const [mode, setMode] = useState ('WELCOME');
+
   const topics=[
     {id:1, title: 'html', body: 'html is ...'},
     {id:2, title: 'css', body: 'css is ...'},
     {id:3, title: 'js', body: 'javascript is ...'},
   ]
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, web"></Article>
+  }
+  else if (mode === 'READ'){
+    content= <Article title="READ" body="Hello, read"></Article>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header')
+        setMode('WELCOME');
       }}></Header>
       <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+        setMode('READ');
       }}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
 
   );
