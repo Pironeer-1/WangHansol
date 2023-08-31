17a0b230ff167abf097dfb1b6c87197b8fd43521
hasoleee <wsw3727@gmail.com>
2023-08-31 15:27:33 +0900

    8. Creat

--- 01react/src/App.js
+++ 01react/src/App.js
@@ -25,7 +25,7 @@ function Nav(props){
       <li key={t.id}>
       <a id={t.id} href={'/read/' + t.id} onClick={(event)=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}
       </a>
       </li>);
@@ -36,13 +36,25 @@ function Nav(props){
     </ol>
   </nav>
 }
+
+function Create(){
+  return <article>
+    <h2>Create</h2>
+    <form>
+      <p><input type="text" name="title" placeholder='title'/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
 function App() {
   //const _mode = useState ('WELCOME'); //초기값
   //const mode = _mode[0]; //상태의 값을 읽음
   //const setMode = _mode[1]; //setmode를 통해서 값을 바꿈
 
   const [mode, setMode] = useState ('WELCOME');
-
+  const [id, setId] = useState (null);
   const topics=[
     {id:1, title: 'html', body: 'html is ...'},
     {id:2, title: 'css', body: 'css is ...'},
@@ -53,17 +65,32 @@ function App() {
     content = <Article title="Welcome" body="Hello, web"></Article>
   }
   else if (mode === 'READ'){
-    content= <Article title="READ" body="Hello, read"></Article>
+    let title, body =null;
+    for(let i = 0; i < topics.length; i++){
+      if(topics[i].id === id){
+        title=topics[i].title;
+        body=topics[i].body;
+      }
+    }
+    content= <Article title={title} body={body}></Article>
+  }
+  else if (mode === 'CREATE'){
+    content= <Create></Create>
   }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
         setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
+      <Nav topics={topics} onChangeMode={(_id)=>{
         setMode('READ');
+        setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
 
   );
