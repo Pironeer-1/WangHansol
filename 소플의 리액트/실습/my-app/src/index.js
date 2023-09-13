466c6bc4ee15e844bf0e87cb957c9afec2b61736
hasoleee <wsw3727@gmail.com>
2023-09-14 01:44:39 +0900

    [react] 7 훅

--- 소플의 리액트/실습/my-app/src/index.js
+++ 소플의 리액트/실습/my-app/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 // import Clock from './chapter_04/Clock';
 import NotificationList from './chapter_06/NotificationList';
-
+import Accommodate from './chapter_07/Accommodate';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>,
 );
 
